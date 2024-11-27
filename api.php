aea16970c1430336bfcb08cc3af3bebf8e1fdfea
maritzacamila11pm <1552554@senati.pe>
2024-11-27 13:46:57 -0500

    27-11-2024

--- api.php
+++ api.php
@@ -1,16 +1,40 @@
 <?php
+//Maritza camila Pongo Mamani
 $metodo = $_SERVER['REQUEST_METHOD'];
 $respuesta =[];
 
 switch ($metodo) {
     case 'GET':
-        $respuesta = ['mensaje' => 'Metodo Get correcto ',
+        $respuesta = [
+        'mensaje' => 'Metodo Get correcto ',
         'data'=> $_GET
     ];
+         break;
+    case 'POST':
+        $data_entrante = json_decode(file_get_contents("php://input"), true);
+
+        $respuesta = [
+            'mensaje' => 'Metodo Post correcto',
+            'data'=> $data_entrante
+        ];
         break;
     
-    default:
-        # code...
-        break;
+        default:
+            # code...
+            break;
+    }
+    
+echo json_encode($respuesta);
+//yes no 
+$metodo = $_SERVER['REQUEST_METHOD'];
+$respuesta =[];
+
+switch ($metodo) {
+    case 'GET':
+        $respuesta = [
+        'mensaje' => 'Metodo Get correcto ',
+        'data'=> $_GET
+    ];
+         break;
 }
 echo json_encode($respuesta);
--- app.js
+++ app.js
@@ -15,9 +15,52 @@ async function getData(){
         console.log("Error al momento de hacer la peticion GET: "+ error);
 
     }
-
 }
 let getdata = document.querySelector('#getdata');
 getdata.addEventListener('click', function(){
     getData();
 });
+//MARITZA CAMILA PONGO MAMANI 
+//ALTER: PARA VER SI YA ESTAS CONECTADO ES SOLO UNA ALERTA DE FUNCOANLIDAD
+async function postData() {
+  //alert('Ingreso a el POST');
+  try {
+    const respuesta = await fetch(apiUrl,{
+         method: "POST",
+         headers: {"Content-Type": "application/json"},
+         body: JSON.stringify({id: 123, nombre: 'Maritza', apellido: 'Pongo',lenguaje_fav:"JAVA", color:"Verde"})
+    });
+
+    const data_retorno = await respuesta.json();
+    console.log(data_retorno);
+
+  } catch (error) {
+    console.log("Ocurrio un error al momento de hacer la peticion POST: "+ error )
+  }
+}
+
+//para yes no 
+const apiUrl1 = "https://yesno.wtf/api";
+
+async function getData(){
+    console.log('Ingreso a get data ');
+    try {
+        const respuesta = await fetch(apiUrl1,{
+            method: "GET"
+        });
+        const data = await respuesta.json();
+        
+        console.log('data');
+        console.log(data);
+
+    }catch(error){
+        console.log("Error al momento de hacer la peticion GET: "+ error);
+
+    }
+}
+let yesno = document.querySelector('#yes');
+yesno.addEventListener('click', function(){
+    getData();
+});
+
+
